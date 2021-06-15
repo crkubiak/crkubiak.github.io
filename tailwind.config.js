13d2876869947ba78e470ef9640e11cd1d039daf
Charles Kubiak <charleskubiak@Charless-MacBook-Pro.local>
2021-06-15 07:58:54 -0500

    responsive?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "rich-black": "#0B0C10",
+        "gun-metal": "#1F2833",
+        silver: "#C5C6C7",
+        "fluorescent-blue": "#66FCF1",
+        "cadet-blue": "#45A29E",
+      },
+    },
   },
   variants: {
     extend: {},
